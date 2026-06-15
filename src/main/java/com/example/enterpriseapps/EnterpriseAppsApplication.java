815d2d7ddb1b11f703552057edfbf484a3f6b209
DTrang0801 <thien.nguyen@student.ehb.be>
2026-06-15 10:13:16 +0200

    Initieel Spring Boot project

---
+++ src/main/java/com/example/enterpriseapps/EnterpriseAppsApplication.java
@@ -0,0 +1,13 @@
+package com.example.enterpriseapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseAppsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseAppsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/enterpriseapps/EnterpriseAppsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.enterpriseapps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseAppsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
