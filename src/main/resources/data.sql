6f2d76106a1ebe2f3f92a36fa31a529b5fe40d17
DTrang0801 <thien.nguyen@student.ehb.be>
2026-06-15 10:47:50 +0200

    Configureer H2 database, mail en voeg testdata toe

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO locations (naam, adres, capaciteit) VALUES ('Gemeenschapscentrum De Rinck', 'Rue du Chapelain 14, 1070 Anderlecht', 150);
+INSERT INTO locations (naam, adres, capaciteit) VALUES ('Campus Kaai', 'Nijverheidskaai 170, 1070 Anderlecht', 200);
+INSERT INTO locations (naam, adres, capaciteit) VALUES ('Parochiezaal Sint-Guido', 'Groot Eiland 6, 1070 Anderlecht', 80);
+
+INSERT INTO events (tijdstip, titel, omschrijving, organisatie, contact_email, locatie_id) VALUES ('2026-07-15 14:00:00', 'Zomerfeest Anderlecht', 'Een gezellige middag met muziek, eten en activiteiten voor het hele gezin.', 'Eigen beheer', 'info@ngo-anderlecht.be', 1);
+INSERT INTO events (tijdstip, titel, omschrijving, organisatie, contact_email, locatie_id) VALUES ('2026-08-20 10:00:00', 'Taalcafé Nederlands', 'Ontmoetingsmoment voor Nederlandsoefening in een ontspannen sfeer.', 'Eigen beheer', 'taal@ngo-anderlecht.be', 2);
+INSERT INTO events (tijdstip, titel, omschrijving, organisatie, contact_email, locatie_id) VALUES ('2026-09-05 18:00:00', 'Informatiesessie Sociale Hulp', 'Sessie over beschikbare sociale diensten en steun in Anderlecht.', 'CPAS Anderlecht', 'cpas@anderlecht.be', 3);
